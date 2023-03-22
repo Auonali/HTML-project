60863bf18f09d35322432d4fc9d4170e3b62f25e
root <auon ali>
2023-03-22 15:43:36 +0000

    HP

---
+++ devops_first.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+echo "Sara: Hi!!"
+
+sleep 5
+
+echo "Rabia: Hey"
+
+sleep 2
+
+echo "Sara: Kesi ho?"
+
+sleep 8
+
+echo "Rabia: mai theak, tum sunaao."
+
+sleep 1
+
+echo "Sara: Mai bhi theak,free ho to ludo khelain???"
+
+sleep 3
+
+echo "Rabia: sabar hamza ki feelings ke sath khel kar aati hu ;P" 
+
+
